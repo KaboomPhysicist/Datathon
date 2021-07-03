c04cdb77f09be365e39b0fb6ab155f24988cae27
KaboomPhysicist <cristian.serna1@udea.edu.co>
2021-07-02 22:12:03 -0500

    minor change

--- model_train/hyperparameters_opt_n2.py
+++ model_train/hyperparameters_opt_n2.py
@@ -21,13 +21,35 @@ def hyperoptimization(epochs,param_grid,type='random'):
     else:
         grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=4, verbose=1, n_jobs=-1)
 
-
     grid_result = grid.fit(X_grav_train, grav_train)
     test_acc = grid.score(X_grav_test, grav_test)
 
     # Save and evaluate results
     with open(output_file, 'a') as f:
-        s = ('Modelo: {}\nBest Accuracy : '
+        s = ('Modelo (Gravedad): {}\nBest Accuracy : '
+            '{:.4f}\n{}\nTest Accuracy : {:.4f}\n\n')
+        output_string = s.format(
+            'Red Neuronal V2',
+            grid_result.best_score_,
+            grid_result.best_params_,
+            test_acc)
+        print(output_string)
+        f.write(output_string)
+
+    model = KerasClassifier(build_fn = n2.create_model2, epochs = epochs,
+                            batch_size = 10, verbose=False)
+
+    if type=='random':        
+        grid = RandomizedSearchCV(estimator=model, param_distributions=param_grid, cv=4, verbose=False, n_iter=5)
+    else:
+        grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=4, verbose=1, n_jobs=-1)
+
+    grid_result = grid.fit(X_ses_train, ses_train)
+    test_acc = grid.score(X_ses_test, ses_test)
+
+    # Save and evaluate results
+    with open(output_file, 'a') as f:
+        s = ('Modelo (Sesgo): {}\nBest Accuracy : '
             '{:.4f}\n{}\nTest Accuracy : {:.4f}\n\n')
         output_string = s.format(
             'Red Neuronal V2',
