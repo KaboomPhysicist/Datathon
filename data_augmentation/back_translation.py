0f51a1518065ccf07d20db2ebc865e29716c6a63
Juan Pablo Ortiz Gil <sjuanortig@gmail.com>
2021-07-06 16:02:44 -0500

    Changed translate library

--- data_augmentation/back_translation.py
+++ data_augmentation/back_translation.py
@@ -1,26 +1,26 @@
 import pandas as pd
-import translators as ts
+from BackTranslation import BackTranslation
 import seaborn as sns
 import matplotlib.pyplot as plt
+from time import sleep
 
 
 CSV_Path ="../data/clasificacion.csv"
 LANG = 'en'
 OLANG = 'es'
+API = 'google'
 
 def translate(df, LANG, OLANG):
     translations = {}
-    back_translations = {}
+    trans = BackTranslation()
     
     df_en = df.copy()
+    trans.raise_Exception = True
 
     for element in df_en['Item (Texto)'].unique():
-        translations[element] =  ts.google(element, OLANG, LANG)
+        sleep(1)
+        translations[element] =  trans.translate(text=element, src=OLANG, tmp = LANG).result_text
     df_en.replace(translations, inplace = True)
-
-    for element in df_en['Item (Texto)'].unique():
-        back_translations[element] =  ts.google(element, LANG, OLANG)
-    df_en.replace(back_translations, inplace = True)
     
     return df_en
     
@@ -29,10 +29,10 @@ def main():
     df['GravedadMode'] = df['Gravedad'].str.split(',',expand=True).mode(axis=1, numeric_only=False, dropna=True)[0]
     df['SesgoMode'] = df['Sesgo'].str.split(',',expand=True).mode(axis=1, numeric_only=False, dropna=True)[0]
     df = df[['Item (Texto)', 'GravedadMode', 'SesgoMode']]
-    df_en = translate(df, LANG, OLANG)
-    df_concat = pd.concat([df, df_en], ignore_index=True, sort=False)
-    df_concat.to_csv(f'clasificacion-{google}-{LANG}.csv')
-    sns.catplot(data = df, x = 'GravedadMode', kind = 'count')
+    
+    df_concat = pd.concat([df, translate(df, LANG, OLANG)], ignore_index=True, sort=False)
+    df_concat.to_csv(f'clasificacion-{API}-{LANG}.csv')
+    sns.catplot(data = df_concat, x = 'GravedadMode', kind = 'count')
     plt.show()
     
 if __name__ == '__main__':
