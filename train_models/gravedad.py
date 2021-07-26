db8a01bb15ea6177fce4f9b241990d18717242ab
AzureUser <azureuser@procesame-este.q1xouvgyyhoudl3334gsexvfzc.xx.internal.cloudapp.net>
2021-07-26 14:06:10 +0000

    Fixed summary()

--- train_models/gravedad.py
+++ train_models/gravedad.py
@@ -139,10 +139,11 @@ def create_model(neurons=20, momentum=0.9):
   mod.compile(optimizer=opt,
                 loss='categorical_crossentropy',
                 metrics=['accuracy'])
+  mod.summary()
   return mod
 
 model = KerasClassifier(build_fn=create_model,epochs=500,batch_size=256, callbacks=[es])
-model.summary()
+
 
 #momentum = [0.1, 0.3, 0.5, 0.7, 0.9, 1]
 #param_grid = dict(momentum = momentum )
